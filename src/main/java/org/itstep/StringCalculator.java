b165bbc831cc95c4edb825f01854364c9f2d59bf
RoVas <brolikolik@gmail.com>
2021-01-12 06:10:35 +0200

    withaut throws

--- src/main/java/org/itstep/StringCalculator.java
+++ src/main/java/org/itstep/StringCalculator.java
@@ -1,39 +1,145 @@
 package org.itstep;
 
+import java.util.Arrays;
+
 public class StringCalculator {
-    private static final int MAXINT=1000;
-    public static int add(String s) {
-        if(s == null || "".equals(s)) {
-            return 0;
+    private static final int MAXINT = 1000;
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+    private static final String EMPTY = "";
+    private static final char DEGREE = '^';
+    private static final char COMMA = ',';
+    private static final String SCOMMA = ",";
+
+
+
+    public static int add(String[] s) {
+        int res=ZERO;
+        for(int i=0;i<s.length;i++){
+            res+=add(s[i]);
         }
+        return res;
+    }
+    public static int add(String s,String h) {
+
+        return add(s)+add(h);
+    }
+    public static int add(String s,String h,String v) {
+
+        return add(s)+add(h)+add(v);
+    }
+    public static int add(String s) {
+        if (s == null || EMPTY.equals(s)) {
+            return ZERO;
+        }
+        s = s.replace(" ", EMPTY);
         String[] snums;
-        if(s.charAt(0)=='/'&&s.charAt(1)=='/'&&s.charAt(3)=='\n'){
-            char razdelitel=s.charAt(2);
-            try {
+        if (s.charAt(0) == '/' && s.charAt(1) == '/') {
+            if (s.charAt(2) == '[') {
+                String[] razdeles=razdels(s);
+                String h="//";
+                for (int i=1;i<razdeles.length;i++) {
+                    h +="["+razdeles[i]+"]";
+                }
+                h+="\n";
+                s=s.replace(h,EMPTY);
+                for(int i=1;i<razdeles.length;i++){
+                    s = s.replace(razdeles[i], SCOMMA);
+                }
+            } else if (s.charAt(3) == '\n') {
+                char razdelitel = s.charAt(2);
+
                 if (Character.isDigit(razdelitel)) {
-                    throw new Exception("SpliterFormatException");
+                    System.out.println("throw SpliterFormatException");
+                    return 0;
                 }
                 s = s.replace("//" + razdelitel + "\n", "");
-                s = s.replace(Character.toString(razdelitel), ",");
-            }
-            catch (Exception ex){
-                System.out.println(ex.getMessage());
+                s = s.replace(Character.toString(razdelitel), SCOMMA);
             }
         }
-
-        s = s.replace("\n", ",");
-        snums = s.split(",");
-
-
-        int sum = 0;
-
-        for (String snum: snums) {
-            if(Integer.parseInt(snum.trim())<=MAXINT) {
+        s = s.replace("\n", SCOMMA);
+        s=stepen(s);
+        snums = s.split(SCOMMA);
+        int sum = ZERO;
+        for (String snum : snums) {
+            if (Integer.parseInt(snum.trim()) <= MAXINT) {
                 sum += Integer.parseInt(snum.trim());
             }
         }
         return sum;
     }
 
+    private static String[] razdels(String str){
+        String[] s=new String[]{EMPTY};
+        int i=ZERO;
+        boolean zapis=false;
+        while (i!=str.length()){
+            if(str.charAt(i)==']'){
+                zapis=false;
+            }
+            if(zapis){
+                s[s.length-1]+=Character.toString(str.charAt(i));
+            }
+            if(str.charAt(i)=='['){
+                zapis=true;
+                s= Arrays.copyOf(s,s.length+ONE);
+                s[s.length-1]=EMPTY;
+            }
+
+
+            i++;
+        }
+        return s;
+
+    }
+    private static  String stepen(String s){
+
+        String m=EMPTY;
+        if (s.charAt(0)!=DEGREE){
+            if(s.length()>1){
+                if(s.charAt(1)!=DEGREE){
+                    m += Character.toString(s.charAt(ZERO));
+                }
+            }
+            else {
+                m += Character.toString(s.charAt(ZERO));
+            }
+        }
+        for(int i=1;i<s.length();i++){
+
+            if(s.charAt(i)!=DEGREE&&s.charAt(i-1)!=DEGREE||s.charAt(i)==COMMA) {
+                if(s.length()>i+1){
+                    if(s.charAt(i+1)!=DEGREE){
+                        m += Character.toString(s.charAt(i));
+                    }
+                }
+                else{
+                    m += Character.toString(s.charAt(i));
+                }
+            }
+            else if(s.charAt(i)==DEGREE){
+                if(s.length()>i+ONE){
+                    if(s.charAt(i+ONE)==COMMA) {
+                        int y=Integer.parseInt(s.charAt(i-ONE) + EMPTY);
+                        y*=y;
+                        m+=Integer.toString(y);
+                    }
+                    else {
+                        int y=Integer.parseInt(s.charAt(i-ONE) + EMPTY);
+                        for(int r=ONE;r<Integer.parseInt(s.charAt(i+ONE)+"");r++){
+                            y*=Integer.parseInt(s.charAt(i-ONE) + EMPTY);
+                        }
+                        m+=Integer.toString(y);
+                    }
+                }
+                else{
+                    int y=Integer.parseInt(s.charAt(i-ONE) + EMPTY);
+                    y*=y;
+                    m+=Integer.toString(y);
+                }
+            }
+        }
+        return m;
+    }
 
 }
--- src/test/java/org/itstep/StringCalculatorTest.java
+++ src/test/java/org/itstep/StringCalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class StringCalculatorTest  {
     @Test
     public void kata01() {
-        Assert.assertEquals(0, StringCalculator.add(null));
+        Assert.assertEquals(0, StringCalculator.add((String) null));
         Assert.assertEquals(0, StringCalculator.add(""));
         Assert.assertEquals(1, StringCalculator.add("1"));
         Assert.assertEquals(3, StringCalculator.add("1,2"));
@@ -39,7 +39,56 @@ public class StringCalculatorTest  {
         Assert.assertEquals(3, StringCalculator.add("//;\n1;2"));
         Assert.assertEquals(5, StringCalculator.add("//*\n2,3"));
         Assert.assertEquals(7, StringCalculator.add("//#\n3#4"));
-//        Assert.assertEquals(2000, StringCalculator.add("1000, 1000"));
-//        Assert.assertEquals(0, StringCalculator.add("1001\n1001"));
+        //Assert.assertEquals( 0, StringCalculator.add("//1\n1 1 1"));
+        //Assert.assertEquals(0, StringCalculator.add("//;\n1#2"));
+    }
+    @Test
+    public void kata06() {
+        Assert.assertEquals(6, StringCalculator.add("//[***]\n1 *** 2 *** 3"));
+        Assert.assertEquals(20, StringCalculator.add("//[xy]\n3xy4xy5xy8"));
+
+    }
+    @Test
+    public void kata07() {
+        Assert.assertEquals(6, StringCalculator.add("// [*] [%] \n1*2%3"));
+        Assert.assertEquals(8, StringCalculator.add("// [&&]  [|||] \n3|||2&&3"));
+
+    }
+    @Test
+    public void kata08() {
+        Assert.assertEquals(14, StringCalculator.add("//[#$]\n1#$2#$3", "// [&&] [|||] \n3 |||2 &&3"));
+        Assert.assertEquals(10, StringCalculator.add("1", "1,2", "1\n2,3"));
+
+    }
+    @Test
+    public void kata09() {
+
+
+    }
+    @Test
+    public void kata10() {
+        Assert.assertEquals(6, StringCalculator.add("// [*] [%] \n1 * 2% 3"));
+        Assert.assertEquals(8, StringCalculator.add("// [&&] [|||]\n3 |||2 &&3"));
+        Assert.assertEquals(7, StringCalculator.add("// [//] \n 1//2//4"));
+        Assert.assertEquals(1998, StringCalculator.add("999,999"));
+        Assert.assertEquals(3, StringCalculator.add("3,1001"));
+        Assert.assertEquals(6, StringCalculator.add("// [*] [%] \n1 * 2% 3"));
+
+    }
+    @Test
+    public void kata11() {
+        Assert.assertEquals(6, StringCalculator.add("2^, 2"));
+        Assert.assertEquals(10, StringCalculator.add("3^\n1"));
+        Assert.assertEquals(8, StringCalculator.add("2^2, 2^2"));
+
+
+    }
+    @Test
+    public void kata12() {
+        Assert.assertEquals(10, StringCalculator.add("2^3, 2"));
+        Assert.assertEquals(31, StringCalculator.add("3^3\n2^2"));
+        Assert.assertEquals(257, StringCalculator.add("//;\n1; 2^8"));
+
+
     }
 }
